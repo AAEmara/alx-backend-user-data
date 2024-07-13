01a01a36b7ccef37baf8881903b306a7f879f4d4
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-13 04:50:56 +0300

    0x01: Task6 Python Script - Basic Authentication.
    Created a Basic Authentication Class `BasicAuth`
    that inherits its methods from the `Auth` Class.
    Extracted the authentication type from the
    environment variables.

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A method that defines Basic Authentication."""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """A class that represents the Basic Authentication.
+    """
+    pass
