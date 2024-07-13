e68f0ec664af5d1f7bcbcc20d1909461041d2350
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-13 05:51:03 +0300

    0x01: Task7 Python Script - Base64 in Basic Auth.
    Applied `Base64 part` extraction from the
    Authorization header in Basic Authentication.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,21 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """A class that represents the Basic Authentication.
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Creates a base64 header for Basic Authentication.
+        Args:
+            authorization_header: Authorization header in the
+            Basic Authentication.
+        Returns:
+            Base64 part of the Authorization Header from the
+            Basic Authentication.
+        """
+        if authorization_header is None:
+            return (None)
+        elif not isinstance(authorization_header, str):
+            return (None)
+        elif "Basic " not in authorization_header:
+            return (None)
+
+        return authorization_header[len("Basic "):]
