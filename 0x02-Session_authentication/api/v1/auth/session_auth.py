d0ed08199713755e7571e3ce299d6518699c5819
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-15 03:19:42 +0300

    0x02: Task3 Python Script - User ID Retrieval.
    Retrieving the `user_id` based on the given
    `session_id`.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,9 +16,23 @@ class SessionAuth(Auth):
             user_id: The ID of the user that the session will be created for.
         Returns:
             None: if the `user_id` is None or not a string.
+            session_id: if the `user_id` is valid.
         """
         if user_id is None or not isinstance(user_id, str):
             return (None)
         session_id: str = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return (session_id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves the User Id based on the given Session Id.
+        Args:
+            session_id: The session's ID.
+        Returns:
+            None: if the `session_id` is None or not a string.
+            user_id: if the `session_id` is valid and available.
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return (None)
+        user_id = self.user_id_by_session_id.get(session_id)
+        return (user_id)
