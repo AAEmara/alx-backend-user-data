f8253b32c97190cf069f4ca6273ac5ad4fbd8434
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-21 20:28:33 +0300

    0x03: Task5 Python Script - Registering a User.
    Registering a User based on the credentials given,
    if the user isn't registered in the database, a
    new user will be created, otherwise a ValueError
    exception will be raised.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """A module that defines authentication functions."""
 
 from bcrypt import gensalt, hashpw
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -18,3 +20,33 @@ def _hash_password(password: str) -> bytes:
     # Hashing the password and salt.
     password_hash = hashpw(password_bytes, salt)
     return (password_hash)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Takes the user credentials and registers a User instance.
+        Args:
+            email: The user's given email.
+            password: The user's given password.
+        Returns:
+            The registered User object.
+        """
+        # Retrieving the User instance if it exists.
+        try:
+            # Searching for the user in the database.
+            user = self._db.find_user_by(email=email)
+            # if the user exists, we will raise a ValueError.
+            if user:
+                raise(ValueError(f"User {email} already exists"))
+        except AttributeError:
+            # if the user doesn't exist, we will hash a password and
+            # create the user.
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+        return (new_user)
