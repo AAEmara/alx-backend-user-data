ebb922cf98e1cfa1532419cdc164a8b892e6ae15
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-23 06:04:54 +0300

    0x03: Task19 Python Script - Update Pwd Endpoint.
    Implemented the Update Password endpoint so the
    user can send his `new_password` `reset_token` and
    `email` to reset the password.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -91,5 +91,23 @@ def get_reset_password_token():
     return (jsonify(message), 200)
 
 
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password():
+    """Updates the user's password.
+    """
+    from sqlalchemy.orm.exc import NoResultFound
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        user = AUTH._db.find_user_by(email=email)
+    except ValueError, NoResultFound:
+        abort(403)
+    AUTH.update_password(reset_token, new_password)
+    message = {"email": email, "message": "Password updated"}
+    return (jsonify(message), 200)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
